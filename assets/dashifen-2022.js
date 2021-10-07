625a6a43d01a6753d222d53f20d7bbaf6118cb3c
David Dashifen Kees <dashifen@dashifen.com>
2021-10-07 12:50:38 -0400

    css and js builds

---
+++ assets/dashifen-2022.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",(()=>{const e=document.getElementsByTagName("html")[0].classList;e.remove("no-js"),e.add("js")}));
+//# sourceMappingURL=dashifen-2022.js.map
---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const {src, dest, watch} = require('gulp'),
+  sourcemaps = require('gulp-sourcemaps'),
+  autoPrefixer = require('autoprefixer'),
+  postcss = require('gulp-postcss'),
+  plumber = require('gulp-plumber'),
+  cssNano = require('cssnano'),
+  sass = require('gulp-sass')(require('sass'));
+
+const css = async function () {
+  return src('assets/styles/dashifen-2022.scss')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(postcss([autoPrefixer(), cssNano()]))
+    .pipe(sourcemaps.write())
+    .pipe(dest('assets/'));
+}
+
+const watcher = async function () {
+  await css();
+  watch('assets/styles/**/*.scss');
+}
+
+exports.css = css;
+exports.build = css;
+exports.watch = watcher;
